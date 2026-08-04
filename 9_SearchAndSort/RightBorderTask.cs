287bb214147d5ca3ee4be48f30558167fb697871
Andrew <andreybybnov435@yandex.ru>
2026-08-04 13:37:45 +0500

    Search And Sort

---
+++ 9_SearchAndSort/RightBorderTask.cs
@@ -0,0 +1,19 @@
+﻿namespace UlearnCourse.SearchAndSort
+{
+    public class RightBorderTask
+    {
+        public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+        {
+            while (left < right - 1)
+            {
+                var m = (left + right) / 2;
+                if (phrases[m].CompareTo(prefix) > 0 && !phrases[m].StartsWith(prefix))
+                    right = m;
+                else
+                    left = m;
+            }
+
+            return right;
+        }
+    }
+}
