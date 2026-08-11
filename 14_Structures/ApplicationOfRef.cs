9af084fc5b30a7a4d1ec0c72f30f1905b910c069
Andrew <andreybybnov435@yandex.ru>
2026-08-11 14:12:50 +0500

    Structures

---
+++ 14_Structures/ApplicationOfRef.cs
@@ -0,0 +1,23 @@
+﻿namespace UlearnCourse.Structures
+{
+    internal class ApplicationOfRef
+    {
+        public static void WriteAllNumbersFromText(string text)
+        {
+            var pos = 0;
+
+            while (true)
+            {
+                SkipSpaces(text, ref pos);
+                var num = ReadNumber(text, ref pos);
+
+                if (num == "") 
+                    break;
+
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 14_Structures/ExperimentsTask.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualBasic;
+
+namespace UlearnCourse.Structures
+{
+    public class Experiments
+    {
+        public static ChartData BuildChartDataForArrayCreation(IBenchmark benchmark, int repetitionsCount)
+        {
+            return BuildChartData(new ArrayCreationFactory(), benchmark, repetitionsCount, "Create array");
+        }
+
+        public static ChartData BuildChartDataForMethodCall(IBenchmark benchmark, int repetitionsCount)
+        {
+            return BuildChartData(new MethodCallFactory(), benchmark, repetitionsCount, "Call method with argument");
+        }
+
+        private static ChartData BuildChartData(ICreationFactory factory, IBenchmark benchmark, int repetitionsCount, string title)
+        {
+            var fieldCounts = Constants.FieldCounts;
+
+            var classesTimes = new List<ExperimentResult>();
+            var structuresTimes = new List<ExperimentResult>();
+
+            foreach (var fieldCount in fieldCounts)
+            {
+                var durationInMs = benchmark.MeasureDurationInMs(factory.GetClassCreation(fieldCount), repetitionsCount);
+                classesTimes.Add(new ExperimentResult(fieldCount, durationInMs));
+            }
+
+            foreach (var fieldCount in fieldCounts)
+            {
+                var durationInMs = benchmark.MeasureDurationInMs(factory.GetStructCreation(fieldCount), repetitionsCount);
+                structuresTimes.Add(new ExperimentResult(fieldCount, durationInMs));
+            }
+
+            return new ChartData
+            {
+                Title = title,
+                ClassPoints = classesTimes,
+                StructPoints = structuresTimes,
+            };
+        }
+    }
+
+    public interface ICreationFactory
+    {
+        public ITask GetClassCreation(int i);
+
+        public ITask GetStructCreation(int i);
+    }
+
+    public class ArrayCreationFactory() : ICreationFactory
+    {
+        public ITask GetClassCreation(int i) => new ClassArrayCreationTask(i);
+
+        public ITask GetStructCreation(int i) => new StructArrayCreationTask(i);
+    }
+
+    public class MethodCallFactory() : ICreationFactory
+    {
+        public ITask GetClassCreation(int i) => new MethodCallWithClassArgumentTask(i);
+
+        public ITask GetStructCreation(int i) => new MethodCallWithStructArgumentTask(i);
+    }
+}
