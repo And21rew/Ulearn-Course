f6479fcce7a8d1b114a3afab6ff3a81b0d136748
Andrew <andreybybnov435@yandex.ru>
2026-08-16 16:26:29 +0500

    Lists And Dictionaries

---
+++ FundamentalsOfProgrammingPart2/2_ListsAndDictionaries/GhostsTask.cs
@@ -0,0 +1,52 @@
+﻿namespace UlearnCourse.FundamentalsOfProgrammingPart2.ListsAndDictionaries
+{
+    public class GhostsTask : IMagic, IFactory<Vector>, IFactory<Segment>, IFactory<Cat>, IFactory<Robot>, IFactory<Document>
+    {
+        private byte[] bytes = { 1, 2, 3 };
+
+        private Vector vector;
+        private Segment segment;
+        private Cat cat;
+        private Robot robot;
+        private Document document;
+
+        public void DoMagic()
+        {
+            vector?.Add(new Vector(1, 1));
+            segment?.Start.Add(new Vector(1, 1));
+            cat?.Rename(" ");
+            Robot.BatteryCapacity++;
+            bytes[0] = 0;
+        }
+
+        Vector IFactory<Vector>.Create()
+        {
+            vector ??= new Vector(0, 0);
+            return vector;
+        }
+
+        Segment IFactory<Segment>.Create()
+        {
+            segment ??= new Segment(new Vector(0, 0), new Vector(1, 1));
+            return segment;
+        }
+
+        Cat IFactory<Cat>.Create()
+        {
+            cat ??= new Cat("", "", DateTime.Now);
+            return cat;
+        }
+
+        Robot IFactory<Robot>.Create()
+        {
+            robot ??= new Robot("");
+            return robot;
+        }
+
+        Document IFactory<Document>.Create()
+        {
+            document ??= new Document("", System.Text.Encoding.ASCII, bytes);
+            return document;
+        }
+    }
+}
---
+++ FundamentalsOfProgrammingPart2/2_ListsAndDictionaries/ReadonlyBytesTask.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+
+namespace UlearnCourse.FundamentalsOfProgrammingPart2.ListsAndDictionaries
+{
+    public class ReadonlyBytes : IEnumerable<byte>
+    {
+        private readonly byte[] bytes;
+        private readonly int cachedHashCode;
+
+        public int Length => bytes.Length;
+
+        public ReadonlyBytes(params byte[] bytes)
+        {
+            if (bytes == null)
+                throw new ArgumentNullException();
+
+            this.bytes = bytes.ToArray();
+
+            unchecked
+            {
+                const int fnvPrime = 16777619;
+                const int fnvOffsetBasis = unchecked((int)2166136261);
+
+                int hash = fnvOffsetBasis;
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    hash ^= bytes[i];
+                    hash *= fnvPrime;
+                }
+                cachedHashCode = hash;
+            }
+        }
+
+        public byte this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Length)
+                    throw new IndexOutOfRangeException();
+
+                return bytes[index];
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != typeof(ReadonlyBytes))
+                return false;
+
+            var objReadonlyBytes = (ReadonlyBytes)obj;
+
+            if (Length != objReadonlyBytes.Length)
+                return false;
+
+            if (ReferenceEquals(this, objReadonlyBytes))
+                return true;
+
+            for (int i = 0; i < Length; i++)
+                if (bytes[i] != objReadonlyBytes.bytes[i])
+                    return false;
+
+            return true;
+        }
+
+        public override int GetHashCode() => cachedHashCode;
+
+        public override string ToString() => $"[{string.Join(", ", bytes)}]";
+
+
+        public IEnumerator<byte> GetEnumerator()
+        {
+            for (int i = 0; i < Length; i++)
+                yield return bytes[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
