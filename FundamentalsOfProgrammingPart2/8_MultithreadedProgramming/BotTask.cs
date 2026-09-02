17d5f8be9a132c44fdfdd3baf213156887042c08
Andrew <andreybybnov435@yandex.ru>
2026-09-02 14:09:06 +0500

    Multithreaded Programming

---
+++ FundamentalsOfProgrammingPart2/8_MultithreadedProgramming/BotTask.cs
@@ -0,0 +1,37 @@
+﻿namespace UlearnCourse.FundamentalsOfProgrammingPart2._8_MultithreadedProgramming
+{
+    public partial class Bot
+    {
+        public Rocket GetNextMove(Rocket rocket)
+        {
+            var tasks = CreateTasks(rocket);
+            var results = Task.WhenAll(tasks).GetAwaiter().GetResult();
+            var bestResult = results.OrderByDescending(r => r.Score).First();
+
+            return rocket.Move(bestResult.Turn, level);
+        }
+
+        public List<Task<(Turn Turn, double Score)>> CreateTasks(Rocket rocket)
+        {
+            var tasks = new List<Task<(Turn, double)>>();
+
+            int iterationsPerThread = iterationsCount / threadsCount;
+            int remainder = iterationsCount % threadsCount;
+
+            for (int i = 0; i < threadsCount; i++)
+            {
+                var currentIterations = iterationsPerThread;
+
+                if (i < remainder)
+                    currentIterations = +1;
+
+                var threadSeed = random.Next();
+                var threadRandom = new Random(threadSeed);
+
+                tasks.Add(Task.Run(() => SearchBestMove(rocket, threadRandom, currentIterations)));
+            }
+
+            return tasks;
+        }
+    }
+}
---
+++ FundamentalsOfProgrammingPart2/8_MultithreadedProgramming/ChannelTask.cs
@@ -0,0 +1,74 @@
+﻿namespace UlearnCourse.FundamentalsOfProgrammingPart2._8_MultithreadedProgramming
+{
+    public class Channel<T> where T : class
+    {
+        private readonly List<T> values = new();
+        /// <summary>
+        /// Возвращает элемент по индексу или null, если такого элемента нет.
+        /// При присвоении удаляет все элементы после.
+        /// Если индекс в точности равен размеру коллекции, работает как Append.
+        /// </summary>
+        public T this[int index]
+        {
+            get
+            {
+                lock (values)
+                {
+                    return index >= Count ? null : values[index];
+                }
+            }
+            set
+            {
+                lock (values)
+                {
+                    if (index == Count)
+                    {
+                        values.Add(value);
+                    }
+                    else if (index < Count)
+                    {
+                        values[index] = value;
+                        values.RemoveRange(index + 1, Count - index - 1);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Возвращает последний элемент или null, если такого элемента нет
+        /// </summary>
+        public T LastItem()
+        {
+            lock (values)
+            {
+                return Count > 0 ? values[^1] : null;
+            }
+        }
+
+        /// <summary>
+        /// Добавляет item в конец только если lastItem является последним элементом
+        /// </summary>
+        public void AppendIfLastItemIsUnchanged(T item, T knownLastItem)
+        {
+            lock (values)
+            {
+                if (LastItem() == knownLastItem)
+                    values.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// Возвращает количество элементов в коллекции
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                lock (values)
+                {
+                    return values.Count;
+                }
+            }
+        }
+    }
+}
